62e6471a4cc2a5fce819b8c58b87d0364a9243e3
gtx1049 <gtx_122@126.com>
2015-06-05 14:55:06 +0800

    填充了基本选择页 修正commit

--- app/src/main/java/com/gtx/eatandplay/ChooseTuan.java
+++ app/src/main/java/com/gtx/eatandplay/ChooseTuan.java
@@ -1,11 +1,8 @@
 package com.gtx.eatandplay;
 
 import android.content.Intent;
-import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
